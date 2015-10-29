66243161d34cf6565f8e5c64dcf2f6ce195e7d60
huyettcp <huyettcp@gmail.com>
2015-10-29 00:05:25 -0400

    all year fields work

--- client/main.js
+++ client/main.js
@@ -6,15 +6,20 @@
 
     console.log(result[1].year);
 
+    Years.insert({
+      year: result[0].year,
+      top_grossing_movie_title: result[0].top_grossing_movie_title,
+      top_grossing_movie_genre: result[0].top_grossing_movie_title,
+      top_grossing_movie_production_budget: result[0].top_grossing_movie_production_budget,
+      top_grossing_movie_gross: result[0].top_grossing_movie_gross
+    })
+
     Session.set("dataTable", result[1].year);
   });
   
-  Template.dataTable.helpers({
+Template.dataTable.helpers({
   rant: function () {
     return Session.get("dataTable")
   }
 })
-
-
-
 
--- server/fixtures.js
+++ server/fixtures.js
@@ -2,6 +2,8 @@
   Meteor.startup(function () {
    var cheerio = Meteor.npmRequire('cheerio');
 
+
+
    Meteor.methods({
       getMovieData: function () {
         result = Meteor.http.get("http://www.the-numbers.com/movies/#tab=year")
@@ -12,11 +14,11 @@
 
         for (year_increment=8; year_increment<43; year_increment++) {
        		var year = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(1)').text();
-       		var title = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(3)').text();
-       		var genre = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(4)').text();
-       		var production_budget = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(5)').text();
-       		var gross = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(6)').text();
-       		movie_data.push({year, title, genre, production_budget, gross})
+       		var top_grossing_movie_title = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(3)').text();
+       		var top_grossing_movie_genre = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(4)').text();
+       		var top_grossing_movie_production_budget = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(5)').text();
+       		var top_grossing_movie_gross = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(6)').text();
+       		movie_data.push({year, top_grossing_movie_title, top_grossing_movie_genre, top_grossing_movie_production_budget, top_grossing_movie_gross})
        	}
 
         return movie_data;
