7066eada1ed750371ba62efaea5482256b1ac418
huyettcp <huyettcp@gmail.com>
2015-10-30 17:29:58 -0400

    Individual year scraping and individual movie page scraping structure

--- client/main.js
+++ client/main.js
@@ -1,23 +1,35 @@
 
-  Meteor.call('getMovieData', function(error, result) {
-    if (error) {
-      console.log("error", error);
-    };
+//   Meteor.call('getMovieData', function(error, result) {
+//     if (error) {
+//       console.log("error", error);
+//     };
 
 
    
-   for(var i=0;i<10;i++){
-    Years.insert({
-      year: result[i].year,
-      top_grossing_movie_title: result[i].top_grossing_movie_title,
-      top_grossing_movie_genre: result[i].top_grossing_movie_title,
-      top_grossing_movie_production_budget: result[i].top_grossing_movie_production_budget,
-      top_grossing_movie_gross: result[i].top_grossing_movie_gross
-    })
-  }
+//    for(var i=0;i<30;i++){
+//     Years.insert({
+//       year: result[i].year,
+//       top_grossing_movie_title: result[i].top_grossing_movie_title,
+//       top_grossing_movie_genre: result[i].top_grossing_movie_title,
+//       top_grossing_movie_production_budget: result[i].top_grossing_movie_production_budget,
+//       top_grossing_movie_gross: result[i].top_grossing_movie_gross
+//     })
+//   }
 
-    Session.set("dataTable", result[i].year);
-  });
+
+
+
+// });
+
+Meteor.call('seedAnnualTopGrossing', function(error, data) {
+  if (error) {
+    console.log('error', error);
+  };
+
+  console.log(data)
+  Session.set("dataTable", data)
+
+});
   
 Template.dataTable.helpers({
   rant: function () {
--- server/fixtures.js
+++ server/fixtures.js
@@ -5,23 +5,40 @@
 
 
    Meteor.methods({
-      getMovieData: function () {
-        result = Meteor.http.get("http://www.the-numbers.com/movies/#tab=year")
-        $ = cheerio.load(result.content);
-        var year_increment 
-        var category_increment = 1 
-    	var movie_data = []
+     //  getMovieData: function () {
+     //    result = Meteor.http.get("http://www.the-numbers.com/movies/#tab=year")
+     //    $ = cheerio.load(result.content);
+     //    var year_increment 
+     //    var category_increment = 1 
+    	// var movie_data = []
 
-        for (year_increment=8; year_increment<43; year_increment++) {
-       		var year = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(1)').text();
-       		var top_grossing_movie_title = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(3)').text();
-       		var top_grossing_movie_genre = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(4)').text();
-       		var top_grossing_movie_production_budget = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(5)').text();
-       		var top_grossing_movie_gross = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(6)').text();
-       		movie_data.push({year, top_grossing_movie_title, top_grossing_movie_genre, top_grossing_movie_production_budget, top_grossing_movie_gross})
-       	}
+     //    for (year_increment=8; year_increment<43; year_increment++) {
+     //   		var year = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(1)').text();
+     //   		var top_grossing_movie_title = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(3)').text();
+     //   		var top_grossing_movie_genre = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(4)').text();
+     //   		var top_grossing_movie_production_budget = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(5)').text();
+     //   		var top_grossing_movie_gross = $('#year > p > table > tr:nth-child('+ year_increment +') > td:nth-child(6)').text();
+     //   		movie_data.push({year, top_grossing_movie_title, top_grossing_movie_genre, top_grossing_movie_production_budget, top_grossing_movie_gross})
+     //   	}
 
-        return movie_data;
+     //    return movie_data;
+     //  },
+      seedAnnualTopGrossing: function () {
+      	var result = Meteor.http.get("http://www.the-numbers.com/market/2014/top-grossing-movies")
+      	$ = cheerio.load(result.content);
+      	var title = $('table > tr:nth-child(2) >td:nth-child(2)').text();
+      	var release_date = $('table > tr:nth-child(2) >td:nth-child(3)').text();
+      	
+      	var title_url = title.replace(/\s+/g, '-').toLowerCase();
+      	
+      	var page_view = Meteor.http.get("http://www.the-numbers.com/movie/" + title_url + "#tab=summary")
+      	$ = cheerio.load(page_view.content);
+      	var production_budget = $('#summary > p > table > tr:nth-child(1) > td:nth-child(2)').text();
+
+
+      	data = {title, release_date, title_url, production_budget}
+      	return data
+
       }
    })
   });
