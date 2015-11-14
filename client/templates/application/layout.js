43c8649ec550a7bd74518621520f6cdb4209dbc2
huyettcp <huyettcp@gmail.com>
2015-11-13 22:56:10 -0500

    reactive data working

---
+++ client/templates/application/layout.js
@@ -0,0 +1,9 @@
+Template.layout.rendered = function () {
+
+
+   $(document).ready(function(){
+    $('ul.tabs').tabs();
+    Session.set('selectedYear', 1998)
+})
+
+};
--- client/templates/movies/movies.js
+++ client/templates/movies/movies.js
@@ -1,75 +1,113 @@
 Template.movies.helpers({
   movies: function() {
-    return Movies.find({release_month: 1}).count()
+    return Movies.find().count()
   }
 });
 
 
-Template.movies.topGenresChart = function() {
-    var movies = Movies.find({release_year: 1995}).fetch()
-    var seriesData = [];
+
+Template.movies.events({
+    "click .tab": function() {
+        word = $(".active").text();
+
+        Session.set('selectedYear', word)
+    }
+})
+
+
+// Template.movies.topGenresChart = function() {
+
+//     Session.get('selectedYear')
+//     var pie_year = Session.get('selectedYear')
+//     var movies = Movies.find({release_year: pie_year}).fetch()
+//     var seriesData = [];
     
-    movies.forEach(function(movie) {
-        var dataPoint = {
-            genre: movie.genre, 
-            domestic_gross: parseInt(movie.domestic_box_office_total)};
-        seriesData.push(dataPoint);
-    });
+//     movies.forEach(function(movie) {
+//         var dataPoint = {
+//             genre: movie.genre, 
+//             domestic_gross: parseInt(movie.domestic_box_office_total)};
+//         seriesData.push(dataPoint);
+//     });
     
-    console.log(seriesData)
+//     console.log(seriesData)
     
-    var gross = alasql('SELECT genre, SUM(domestic_gross) AS gross FROM ? GROUP BY genre', [seriesData]);    
+//     var gross = alasql('SELECT genre, SUM(domestic_gross) AS gross FROM ? GROUP BY genre ORDER BY gross DESC', [seriesData]);    
 
-    var data = [{a:1,b:10}, {a:2,b:20}, {a:1,b:30}];
+//     var data = [{a:1,b:10}, {a:2,b:20}, {a:1,b:30}];
     
-    var res = alasql('SELECT a, SUM(b) AS b FROM ? GROUP BY a',[data]);    
+//     var res = alasql('SELECT a, SUM(b) AS b FROM ? GROUP BY a',[data]);    
 
-    console.log(res); // [{"a":1,"b":40},{"a":2,"b":20}]
+//     console.log(res); // [{"a":1,"b":40},{"a":2,"b":20}]
     
-    console.log(gross[1]);
+//     console.log(gross[1]);
 
-    final_data = []
+//     final_data = []
 
-    for (var i = 0; i < gross.length; i++) {
+//     for (var i = 0; i < gross.length; i++) {
 
 
-            final_data.push([gross[i].genre, gross[i].gross]);
+//             final_data.push([gross[i].genre, gross[i].gross]);
 
    
-    }
+//     }
 
-    console.log(final_data)
+//     console.log(final_data)
 
-    return {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
-        },
-        title: {
-            text: "Year's top genres"
-        },
-        tooltip: {
-            pointFormat: '<b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    },
-                    connectorColor: 'silver'
-                }
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'genre',
-            data: final_data
-        }]
-    };
-};
+//     return {
+//         chart: {
+//             plotBackgroundColor: null,
+//             plotBorderWidth: null,
+//             plotShadow: false
+//         },
+//         title: {
+//             text: "Year's top genres"
+//         },
+//         tooltip: {
+//             pointFormat: '<b>{point.percentage:.1f}%</b>'
+//         },
+//         plotOptions: {
+//             pie: {
+//                 allowPointSelect: true,
+//                 cursor: 'pointer',
+//                 dataLabels: {
+//                     enabled: true,
+//                     format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+//                     style: {
+//                         color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+//                     },
+//                     connectorColor: 'silver'
+//                 }
+//             }
+//         },
+//         series: [{
+//             type: 'pie',
+//             name: 'genre',
+//             data: final_data
+//         }]
+//     };
+
+// };
+
+
+function builtPie() {
+    Session.get('selectedYear')
+    Session.get('movie_pie_data')
+    var movie_pie_data = Session.get('movie_pie_data')
+    console.log(movie_pie_data)
+
+
+}
+
+Template.movies.rendered = function() {  
+    this.autorun(function () {  
+        Session.get('selectedYear')
+        var year = Session.get('selectedYear')
+        var pie_year = parseInt(year)
+        var movies = Movies.find({release_year: pie_year}).fetch()
+        Session.set('movie_pie_data', movies)
+        builtPie();
+    });
+
+}
+
+
