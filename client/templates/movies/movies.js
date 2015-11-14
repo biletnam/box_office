cb20eb5a69f5cf5ba81c8e123cdaed2b992074a3
huyettcp <huyettcp@gmail.com>
2015-11-14 12:37:43 -0500

    pie chart is now fully reactive

--- client/templates/movies/movies.js
+++ client/templates/movies/movies.js
@@ -95,7 +95,67 @@ function builtPie() {
     var movie_pie_data = Session.get('movie_pie_data')
     console.log(movie_pie_data)
 
+        var seriesData = [];
+    
+    movie_pie_data.forEach(function(movie) {
+        var dataPoint = {
+            genre: movie.genre, 
+            domestic_gross: parseInt(movie.domestic_box_office_total)};
+        seriesData.push(dataPoint);
+    });
+    
+    console.log(seriesData)
 
+
+    var gross = alasql('SELECT genre, SUM(domestic_gross) AS gross FROM ? GROUP BY genre ORDER BY gross DESC', [seriesData]);    
+    
+    console.log(gross[1]);
+
+    final_data = []
+
+    for (var i = 0; i < gross.length; i++) {
+
+
+            final_data.push([gross[i].genre, gross[i].gross]);
+
+   
+    }
+
+    console.log(final_data)
+
+    $('#container-pie').highcharts({
+
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: this.username + "'s top genres"
+        },
+        tooltip: {
+            pointFormat: '<b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    },
+                    connectorColor: 'silver'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'genre',
+            data: final_data
+        }]
+    })
 }
 
 Template.movies.rendered = function() {  
