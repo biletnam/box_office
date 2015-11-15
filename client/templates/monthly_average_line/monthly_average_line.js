85ef4f9c670fb50b3998354fdcd61cbc46e87962
huyettcp <huyettcp@gmail.com>
2015-11-14 19:36:07 -0500

    data works in line chart

--- client/templates/monthly_average_line/monthly_average_line.js
+++ client/templates/monthly_average_line/monthly_average_line.js
@@ -19,11 +19,11 @@ function buildMonthlyAverageLine() {
 
     var staging_year = _.groupBy(averaged_data, 'release_year');
 
-    console.log(staging_year)
+
 
     var month_array = _.pluck(averaged_data, 'average_box_office_total', 'release_year');
 
-    console.log(month_array)
+
 
 
      final_data = []
@@ -32,9 +32,6 @@ function buildMonthlyAverageLine() {
         final_data.push({name: key});
 
     }
-    var list =  [{food: 'apple', type: 'fruit'},
-                {food: 'potato', type: 'vegetable'},
-                {food: 'banana', type: 'fruit'}]
    
     var apple =  _.chain(averaged_data)
         .groupBy('release_year')
@@ -45,7 +42,9 @@ function buildMonthlyAverageLine() {
         }
         })
         .value();
-    console.log(apple)
+
+    var shit = apple
+    console.log(shit)
 
 
    // var test = _.map(_.where(averaged_data, {release_year: 1995}),
@@ -93,19 +92,7 @@ function buildMonthlyAverageLine() {
             verticalAlign: 'middle',
             borderWidth: 0
         },
-        series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }, {
-            name: 'New York',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-        }, {
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-        }, {
-            name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-        }]
+        series: shit
     });
 };
 
