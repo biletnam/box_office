edaa6939c4d8fad8d0647de1cfe783c3d0785404
huyettcp <huyettcp@gmail.com>
2015-11-18 23:08:19 -0500

    keyword row

---
+++ client/templates/keyword_row/keyword_row.js
@@ -0,0 +1,6 @@
+Template.keywords_row.helpers({
+	gross: function() {
+		return Movies.find()
+	}
+
+})
--- client/templates/keywords/keywords.js
+++ client/templates/keywords/keywords.js
@@ -2,26 +2,34 @@ Template.keywords.rendered = function() {
 
         // var keywords = Movies.find({keyword_array: {$in: ['Unexpected Families']}}).count()
 
-      	var keywords = Keywords.find().fetch()
-        console.log(keywords)
-        var keyword_array = []
+      	// var keywords = Keywords.find().fetch()
+       //  console.log(keywords)
+       //  var keyword_array = []
 
-        keywords.forEach(function(keyword) {
-        	keywords = Movies.find({keyword_array: {$in: [keyword.keyword]}}).count()
-        	var dataPoint = {
-            	keyword: keyword.keyword,
-            	count: keywords
-        	} 
-        	if (keyword.keyword != "" ) {
-        		keyword_array.push(dataPoint);
-        		Keywords.update(keyword._id, 
-        			{$set:{keyword_count: keywords} 
-        		})
-        	}
-        })
+       //  keywords.forEach(function(keyword) {
+       //  	keywords = Movies.find({keyword_array: {$in: [keyword.keyword]}}).count()
+       //  	var dataPoint = {
+       //      	keyword: keyword.keyword,
+       //      	count: keywords
+       //  	} 
+       //  	if (keyword.keyword != "" ) {
+       //  		keyword_array.push(dataPoint);
+       //  		Keywords.update(keyword._id, 
+       //  			{$set:{keyword_count: keywords} 
+       //  		})
+       //  	}
+       //  })
 
-        var yes = _.sortBy(keyword_array, 'count').reverse()
-        console.log(yes)
+       //  var yes = _.sortBy(keyword_array, 'count').reverse()
+       //  console.log(yes)
+
 }
 
 
+
+Template.keywords.helpers({
+	keywords: function() {
+		return Keywords.find({}, {sort:{keyword_count: -1}})
+	}
+
+})
