dbb91a39d1de16346e4fce0b9e29298dee7d1676
huyettcp <huyettcp@gmail.com>
2015-11-30 21:05:59 -0500

    Movie star page

--- client/templates/actor_page/actor_page.js
+++ client/templates/actor_page/actor_page.js
@@ -1,7 +1,7 @@
 Template.actorPage.helpers({
     movies: function() {
 
-        var movies_data = Movies.find()
+        var movies_data = Movies.find({}, {sort: {release_year: -1}})
         return movies_data
 	}
 })
