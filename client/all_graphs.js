e8ee628eaf03edc30dc391e04649d0b1cc5f786b
huyettcp <huyettcp@gmail.com>
2015-12-03 00:36:25 -0500

    refactor actor pages

--- client/all_graphs.js
+++ client/all_graphs.js
@@ -1,3 +1,82 @@
+buildActorGraph = function(actorGraphData)  {
+    var movies = actorGraphData
+    var movie_titles = []
+    var production_budgets = []
+    var total_domestic_grosses = []
+
+    movies.forEach(function(movie) {
+        var movie_title = movie.movie_title
+        var movie_release_year = movie.release_year
+        var inflation_year = Years.findOne({year_int: movie_release_year})
+        var production_budget = movie.production_budget * inflation_year.inflation_rate
+        var total_domestic_gross = movie.domestic_box_office_total * inflation_year.inflation_rate
+ 
+        movie_titles.push(movie_title)
+        production_budgets.push(production_budget)
+        total_domestic_grosses.push(total_domestic_gross)
+
+
+     });
+
+    $('#container_actor_graph').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Movie Budget vs Total Domestic Gross'
+        },
+        xAxis: {
+            categories: movie_titles
+        },
+        yAxis: [{
+            min: 0,
+            title: {
+                text: ''
+            }
+        }, {
+            title: {
+                text: ''
+            },
+            opposite: false
+        }],
+        legend: {
+            shadow: false
+        },
+        tooltip: {
+            shared: true
+        },
+        plotOptions: {
+            column: {
+                grouping: false,
+                shadow: false,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Production Budget',
+            color: 'rgba(248,161,63,1)',
+            data: production_budgets,
+            tooltip: {
+                valuePrefix: '$',
+                valueSuffix: ' M'
+            },
+            pointPadding: 0.3,
+            // pointPlacement: 0.2,
+            yAxis: 1
+        }, {
+            name: 'Total Domestic Gross',
+            color: 'rgba(186,60,61,.9)',
+            data: total_domestic_grosses,
+            tooltip: {
+                valuePrefix: '$',
+                valueSuffix: ' M'
+            },
+            pointPadding: 0.4,
+            // pointPlacement: 0.2,
+            yAxis: 1
+        }]
+    });
+};
 
 buildFranchisePageGraph = function(movieData)  {
 
--- client/templates/actor_graph/actor_graph.js
+++ client/templates/actor_graph/actor_graph.js
@@ -1,103 +1,16 @@
 Template.actorGraph.helpers({
     movies: function() {
 
-        var movies_data = Movies.find({}, {sort: {release_year: -1}})
-        return movies_data
+        var moviesData = Movies.find({}, {sort: {release_year: -1}})
+        return moviesData
 	}
 });
 
-function buildActorGraph()  {
-	var movies = Session.get('actor_graph_session')
-	var movie_titles = []
-	var production_budgets = []
-	var total_domestic_grosses = []
-
-
-	movies.forEach(function(movie) {
-  
-   
-		var movie_title = movie.movie_title
-		var movie_release_year = movie.release_year
-		var inflation_year = Years.findOne({year_int: movie_release_year})
-		
-		var production_budget = movie.production_budget * inflation_year.inflation_rate
-		var total_domestic_gross = movie.domestic_box_office_total * inflation_year.inflation_rate
- 
-
- 
-	
-		movie_titles.push(movie_title)
-		production_budgets.push(production_budget)
-		total_domestic_grosses.push(total_domestic_gross)
-
-
-     });
-
-    $('#container_actor_graph').highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Movie Budget vs Total Domestic Gross'
-        },
-        xAxis: {
-            categories: movie_titles
-        },
-        yAxis: [{
-            min: 0,
-            title: {
-                text: ''
-            }
-        }, {
-            title: {
-                text: ''
-            },
-            opposite: false
-        }],
-        legend: {
-            shadow: false
-        },
-        tooltip: {
-            shared: true
-        },
-        plotOptions: {
-            column: {
-                grouping: false,
-                shadow: false,
-                borderWidth: 0
-            }
-        },
-        series: [{
-            name: 'Production Budget',
-            color: 'rgba(248,161,63,1)',
-            data: production_budgets,
-            tooltip: {
-                valuePrefix: '$',
-                valueSuffix: ' M'
-            },
-            pointPadding: 0.3,
-            // pointPlacement: 0.2,
-            yAxis: 1
-        }, {
-            name: 'Total Domestic Gross',
-            color: 'rgba(186,60,61,.9)',
-            data: total_domestic_grosses,
-            tooltip: {
-                valuePrefix: '$',
-                valueSuffix: ' M'
-            },
-            pointPadding: 0.4,
-            // pointPlacement: 0.2,
-            yAxis: 1
-        }]
-    });
-};
 
 Template.actorGraph.rendered = function() {  
     this.autorun(function () {  
-    	var actor_graph_session = Movies.find({}, {sort: {release_year: 1}}).fetch()
-    	Session.set("actor_graph_session", actor_graph_session)
-    	buildActorGraph()
+    	var actorGraphData = Movies.find({}, {sort: {release_year: 1}}).fetch()
+    	buildActorGraph(actorGraphData)
 
     })
     
