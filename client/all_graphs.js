80b2475f25992e7ea31b982c7402de2512a76977
huyettcp <huyettcp@gmail.com>
2015-12-02 22:55:10 -0500

    Created seperate file to house all of the graph creation functions

---
+++ client/all_graphs.js
@@ -0,0 +1,82 @@
+
+buildFranchisePageGraph = function(movieData)  {
+
+	// var movies = Session.get('franchiseGraphSession')
+	var movie_titles = []
+	var production_budgets = []
+	var total_domestic_grosses = []
+
+	movieData.forEach(function(movie) {
+  
+
+		var movie_title = movie.movie_title
+		var movie_release_year = movie.release_year
+		var inflation_year = Years.findOne({year_int: movie_release_year})
+		
+		var production_budget = movie.production_budget * inflation_year.inflation_rate
+		var total_domestic_gross = movie.domestic_box_office_total * inflation_year.inflation_rate
+ 
+
+ 
+	
+		movie_titles.push(movie_title)
+		production_budgets.push(production_budget)
+		total_domestic_grosses.push(total_domestic_gross)
+	
+
+
+     });
+
+    $('#container_franchise_graph').highcharts({
+
+        chart: {
+            polar: true,
+            type: 'line'
+        },
+
+        title: {
+            text: '',
+            x: -80
+        },
+
+        pane: {
+            size: '80%'
+        },
+
+        xAxis: {
+            categories: movie_titles,
+            tickmarkPlacement: 'on',
+            lineWidth: 0
+        },
+
+        yAxis: {
+            gridLineInterpolation: 'polygon',
+            lineWidth: 0,
+            min: 0
+        },
+
+        tooltip: {
+            shared: true,
+            pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
+        },
+
+        legend: {
+            align: 'right',
+            verticalAlign: 'top',
+            y: 100,
+            layout: 'vertical'
+        },
+
+        series: [{
+            name: 'Production Budget',
+            data: production_budgets,
+            pointPlacement: 'on'
+        }, {
+            name: 'Total Domestic Gross',
+            data: total_domestic_grosses,
+            pointPlacement: 'on'
+        }]
+
+    });
+
+};
