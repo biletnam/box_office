83c815cebc477f3bd57480061944d302f07627ad
huyettcp <huyettcp@gmail.com>
2015-12-08 20:36:13 -0500

    removed useless pie chart

--- client/all_graphs.js
+++ client/all_graphs.js
@@ -406,3 +406,63 @@ buildMonthlyAverageLine = function(movies) {
     });
 
 };
+
+buildPie = function() {
+    Session.get('movie_pie_data')
+    var movie_pie_data = Session.get('movie_pie_data')
+    var year_selected = Session.get('selectedYear')
+    var seriesData = [];
+    
+    movie_pie_data.forEach(function(movie) {
+        var dataPoint = {
+            genre: movie.genre, 
+            domestic_gross: parseInt(movie.domestic_box_office_total)};
+        seriesData.push(dataPoint);
+    });
+
+    var gross = alasql('SELECT genre, SUM(domestic_gross) AS gross FROM ? GROUP BY genre ORDER BY gross DESC', [seriesData]);    
+    
+    final_data = []
+
+    for (var i = 0; i < gross.length; i++) {
+
+
+            final_data.push([gross[i].genre, gross[i].gross]);
+
+   
+    }
+
+    $('#container-pie').highcharts({
+
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: year_selected + " Genre Breakdown"
+        },
+        tooltip: {
+            pointFormat: '<b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    },
+                    connectorColor: 'silver'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'genre',
+            data: final_data
+        }]
+    })
+}
