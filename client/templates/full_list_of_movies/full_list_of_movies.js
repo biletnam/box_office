d3b763c9a194ce6a4924e26b66b8679ebd826a53
huyettcp <huyettcp@gmail.com>
2015-12-12 11:52:36 -0500

    sortable movie table

--- client/templates/full_list_of_movies/full_list_of_movies.js
+++ client/templates/full_list_of_movies/full_list_of_movies.js
@@ -1,8 +1,36 @@
+Template.fullListOfMovies.rendered = function() {  
+    this.autorun(function () {
+      var route = Router.current().route.getName()
+      if (route == 'alphaListOfMovies') {
+         var state = 'Alphabetical (A-Z)'
+      } else if (route == 'genreListOfMovies') {
+         var state = 'Genre (A-Z)'
+      } else if (route == 'ratingListOfMovies') {
+         var state = 'Rating (A-Z)'
+      } else {
+        var state = 'Domestic Box Office (Highest First)'
+      }
+      
+      Session.set('movieListDropDown', state)
+    });
+
+}
+
+
+
+
 Template.fullListOfMovies.helpers({
-	// movies: function() {
- //   		var moviesData = Movies.find()
- //        return moviesData
-	// }
+  sort: function() {  
+    var sort = Session.get('movieListDropDown')
+    return sort
+  }
 });
 
 
+Template.fullListOfMovies.events({
+  'change #movie_sort_dropdown': function (e) {
+    var route = $(event.target).val(); 
+    Router.go(route)
+
+  }
+});
